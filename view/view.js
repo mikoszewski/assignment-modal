5a4dec2a1bdeaf588c96be0704b8a0ade15243ea
mikoszewski <obiboy@interia.pl>
2021-04-13 08:27:37 +0200

    view update

--- view/view.js
+++ view/view.js
@@ -2,9 +2,9 @@ class VendorList {
   //function that renders each element of the Vendor List including: name/policy url and acceptance check box
   renderList(element) {
     return `
-    <li id="vendors">${element.name}
+    <li id="vendors"><div style="display: inline-block; width: 50%;">${element.name}</div>
         <input type="checkbox" class="tab" data-id=${element.id}>Accept</input>
-        <a href=${element.url} class="tab">Link to Vendor policy</a>
+        <a style="float: right;" href=${element.url} class="tab">Link to Vendor policy</a>
     </li>`;
   }
 }
